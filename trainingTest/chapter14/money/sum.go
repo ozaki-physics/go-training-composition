8ebc4c262b96a4970eccbea8a321d6c010825e17
ozaki-physics <setuna.f.masato@gmail.com>
2022-02-05 15:42:07 +0900

    feat: TDD の書籍 第14章 学習用テストと回帰テスト

---
+++ trainingTest/chapter14/money/sum.go
@@ -0,0 +1,13 @@
+package money
+
+type Sum struct {
+	Augend Money
+	Addend Money
+}
+
+// Reduce リデュース 縮小するとかのニュアンス
+// Sum 型 では 2つの Money が含まれているから 圧縮して1個の Money になるニュアンスは遠からず
+func (s Sum) Reduce(b *Bank, to string) Money {
+	amount := s.Augend.Amount() + s.Addend.Amount()
+	return CreateMoney(amount, to)
+}

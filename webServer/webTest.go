209819daba69d328fdd4b50020572562cfaa1fde
ozaki-physics <setuna.f.masato@gmail.com>
2022-05-31 23:15:20 +0900

    feat: httptest のサンプル #32

--- webServer/webTest.go
+++ webServer/webTest.go
@@ -2,9 +2,30 @@ package webServer
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"net/http/httptest"
 )
 
 func TryTestServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello world !")
+}
+
+// TryResponseRecorder
+// See: https://pkg.go.dev/net/http/httptest#example-ResponseRecorder
+func TryResponseRecorder() {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := io.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
 }

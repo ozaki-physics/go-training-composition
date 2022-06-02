fb20c74a2346aebb10e239209441d7561e760384
ozaki-physics <setuna.f.masato@gmail.com>
2022-06-02 22:39:45 +0900

    feat: httptest のサンプル 03 #32

--- webServer/webTest.go
+++ webServer/webTest.go
@@ -52,3 +52,24 @@ func TryServer() {
 	fmt.Printf("%s\n", greeting)
 }
 
+// TryServerHttp2
+// See: https://pkg.go.dev/net/http/httptest#example-Server-HTTP2
+func TryServerHttp2() {
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s", r.Proto)
+	}))
+	ts.EnableHTTP2 = true
+	ts.StartTLS()
+	defer ts.Close()
+
+	res, err := ts.Client().Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	greeting, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", greeting)
+}
